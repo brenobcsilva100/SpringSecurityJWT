59324292899dd8167d08212cb64b0ed0558523c1
Breno <breno.csilva@hotmail.com>
2023-06-27 09:57:40 -0300

    Criando router angular

--- Front/frontend/src/app/app.module.ts
+++ Front/frontend/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { ModuleWithProviders } from "@angular/core
+
+export const appRouters: Routes=[
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: AppComponent }
+]
+
+export const routing : ModuleWithProviders<unknown> = RouterModule.forRoot(appRouters);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
